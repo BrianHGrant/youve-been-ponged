124332f38635009ed2f30b971631706fbad4a29b
BrianHGrant <bhgrant8@gmail.com>
2016-06-10 11:01:11 -0700

    combine divisible by 3 and 5 functions into one, remove merge function. properly display error message in list when invalid data is entered

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,12 @@ var pingedPonged = function(number) {
   var isValid = validPositiveNumber(number);
   if (!isValid) {
     var errorMsg = 'Input is not valid. Please enter a postive integer.';
-    return errorMsg;
+    var pingedPongedArray =[];
+    pingedPongedArray.push(errorMsg);
+    return pingedPongedArray;
   }
   else {
-    var pingedPongedArray = pingPong(pingFunction(number), pongFunction(number));
+    var pingedPongedArray = pingPong(number);
     return pingedPongedArray;
   };
 };
@@ -23,51 +25,24 @@ var validPositiveNumber = function(number) {
 };
 
 // DIVISIBLE BY 3 FUNCTION
-var pingFunction = function(number) {
+var pingPong = function(number) {
   var pingArray = [];
   for (i=1; i<=number; i++) {
-    if (i % 3 != 0) {
+    if ((i % 3 != 0) && (i % 5 != 0)) {
       pingArray.push(i);
     }
-    else {
+    else if ((i % 3 === 0) && (i % 5 != 0)) {
       pingArray.push("ping");
+    }
+    else if ((i % 3 != 0) && (i % 5 === 0)) {
+      pingArray.push("pong");
+    }
+    else if ((i % 3 === 0) && (i % 5 === 0)) {
+      pingArray.push("pingpong");
     }
   };
   return pingArray;
 };
-
-// DIVISIBLE BY 5 FUNCTION
-var pongFunction = function(number) {
-  var pongArray = [];
-  for (i=1; i<=number; i++) {
-    if (i % 5 != 0) {
-      pongArray.push(i);
-    }
-    else {
-      pongArray.push("pong");
-    }
-  };
-  return pongArray;
-};
-
-// MERGE 2 ARRAYS FUNCTION
-var pingPong = function(inArray1, inArray2) {
-  var finalArray = [];
-  for (i=0; i<inArray1.length; i++) {
-    if (inArray1[i]===inArray2[i]) {
-      finalArray.push(inArray1[i]);
-    } else if (parseInt(inArray1[i].toString())) {
-      finalArray.push(inArray2[i]);
-    } else if (parseInt(inArray2[i].toString())) {
-      finalArray.push(inArray1[i]);
-    } else {
-      finalArray.push(inArray1[i] + inArray2[i]);
-    };
-  };
-  return finalArray;
-};
-
-
 
 // USER INTERFACE
 $(document).ready(function() {
