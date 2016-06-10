bf9ba89d345bf2ee7a3c7348dd383ff8ee3f48b5
BrianHGrant <bhgrant8@gmail.com>
2016-06-10 13:54:36 -0700

    add pingPongGame function to allow user to guess number of pings, pongs, and pingpongs, also adds style elements to user interface.

--- js/scripts.js
+++ js/scripts.js
@@ -24,24 +24,49 @@ var validPositiveNumber = function(number) {
   };
 };
 
-// DIVISIBLE BY 3 FUNCTION
+// POPULATE RESULT ARRAY FUNCTION
 var pingPong = function(number) {
-  var pingArray = [];
+  var pingPongArray = [];
+  pingCounter = 0;
+  pongCounter = 0;
+  pingPongCounter = 0;
   for (i=1; i<=number; i++) {
     if ((i % 3 != 0) && (i % 5 != 0)) {
-      pingArray.push(i);
+      pingPongArray.push(i);
     }
     else if ((i % 3 === 0) && (i % 5 != 0)) {
-      pingArray.push("ping");
+      pingPongArray.push("ping");
+      pingCounter ++;
     }
     else if ((i % 3 != 0) && (i % 5 === 0)) {
-      pingArray.push("pong");
+      pingPongArray.push("pong");
+      pongCounter ++;
     }
     else if ((i % 3 === 0) && (i % 5 === 0)) {
-      pingArray.push("pingpong");
+      pingPongArray.push("pingpong");
+      pingPongCounter ++;
     }
   };
-  return pingArray;
+  return pingPongArray;
+};
+
+var pingPongGame = function(pings, pingsGuess, pongs, pongsGuess, pingPongs, pingPongsGuess) {
+  var wins = 0;
+  ifWonStatus = "";
+  if (pingsGuess === pings) {
+    wins ++;
+  };
+  if (pongsGuess === pongs) {
+    wins++;
+  };
+  if (pingPongsGuess === pingPongs) {
+    wins++;
+  };
+  if (wins >= 2) {
+    ifWonStatus = "winner!"
+  } else {
+    ifWonStatus = "loser!"
+  };
 };
 
 // USER INTERFACE
@@ -49,11 +74,24 @@ $(document).ready(function() {
   $('#userInput').submit(function(event) {
     $('#resultList ul').html("");
     var inNumberInt = parseInt($('input#inNumber').val());
-    var results = pingedPonged(inNumberInt);
+    var inName = $('input#inName').val();
+    var pingGuess = parseInt($('input#inPing').val());
+    var pongGuess = parseInt($('input#inPong').val());
+    var pingPongGuess = parseInt($('input#inPingPong').val());
     var htmlOutput = "";
+
+    var results = pingedPonged(inNumberInt);
     results.map(function(result) {
       $('#resultList ul').append('<li>'+result+'</li>');
+      event.preventDefault();
     });
+    $('#instructions').addClass('hide');
+    $('#resultList').removeClass('hide');
+    var winner = pingPongGame(pingCounter, pingGuess, pongCounter, pongGuess, pingPongCounter, pingPongGuess);
+    alert(winner);
+    $('#winnerStatus').removeClass('hide');
+    $('.playerName').text(inName);
+    $('.ifWon').text(ifWonStatus);
     event.preventDefault();
   });
 
