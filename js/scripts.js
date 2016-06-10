4fa2fad206f1f0bdbf2e627b8d460741929aa131
BrianHGrant <bhgrant8@gmail.com>
2016-06-10 09:12:32 -0700

    add validation check for a positive integer.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,24 @@
+var validPositiveNumber = function(number) {
+  if (!number || number <= 0) {
+    return false;
+  } else {
+    return true;
+  };
+};
+
+
+
 $(document).ready(function() {
   $('#userInput').submit(function(event) {
     var inNumberInt = parseInt($('input#inNumber').val());
 
+    var isValid = validPositiveNumber(inNumberInt);
+    if (isValid === false) {
+      alert("Input is not valid. Please enter a postive integer.");
+    }
+    else {
+      alert("Valid positive integer!");
+    };
     alert(inNumberInt);
 
     event.preventDefault();
